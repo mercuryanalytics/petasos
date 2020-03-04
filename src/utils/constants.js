2f13f942fc76b8f70bad89d303ceda21234f2a9a
acatalin92 <a.catalin92@gmail.com>
2020-03-04 13:44:23 +0200

    heroku fake urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,10 +4,9 @@ const prod = {
   APP_URL: 'https://mercury-analytics-frontend.herokuapp.com',
   API_URL: 'https://mercury-analytics-api.herokuapp.com/api/v1',
 };
-
 const dev = {
-  APP_URL: 'http://localhost:3000',
-  API_URL: 'http://localhost:3002',
+  APP_URL: 'https://mercury-analytics-frontend.herokuapp.com',
+  API_URL: 'https://mercury-analytics-api.herokuapp.com/api/v1',
 };
 
 const Constants = env !== 'development' ? prod : dev;
