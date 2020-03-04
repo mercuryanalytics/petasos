de3e077786cf3d530f798ef27f7af4d7a8c0b403
acatalin92 <a.catalin92@gmail.com>
2020-03-04 13:25:46 +0200

    API url update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV;
 
 const prod = {
   APP_URL: 'https://mercury-analytics-frontend.herokuapp.com',
-  API_URL: 'https://mercury-analytics-api.herokuapp.com',
+  API_URL: 'https://mercury-analytics-api.herokuapp.com/api/v1',
 };
 
 const dev = {
