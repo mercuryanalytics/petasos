eadad126070e58da43dfca26527daf433fa02eeb
Relu Burlan <aurelian.burlan@e-spres-oh.com>
2020-03-04 09:25:54 +0200

    Setup the constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,12 +2,14 @@ let Constants, env = process.env.NODE_ENV;
 
 const prod = {
   APP_URL: 'https://mercury-analytics-frontend.herokuapp.com',
-  API_URL: 'http://localhost:3002',
+  API_URL: 'https://mercury-analytics-api.herokuapp.com',
 };
 
 const dev = {
-  APP_URL: 'https://mercury-analytics-frontend.herokuapp.com',
+  APP_URL: 'http://localhost:3000',
   API_URL: 'http://localhost:3002',
 };
 
 export default Constants = env !== 'development' ? prod : dev;;
+
+
