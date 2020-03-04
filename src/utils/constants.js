d1c0aab567e89d91f27da6b7e90fb096c953bc25
acatalin92 <a.catalin92@gmail.com>
2020-03-04 14:30:21 +0200

    done testing heroku

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,9 @@ const prod = {
   APP_URL: 'https://mercury-analytics-frontend.herokuapp.com',
   API_URL: 'https://mercury-analytics-api.herokuapp.com/api/v1',
 };
+
 const dev = {
-  APP_URL: 'https://mercury-analytics-frontend.herokuapp.com',
+  APP_URL: 'http://localhost:3000',
   API_URL: 'https://mercury-analytics-api.herokuapp.com/api/v1',
 };
 
