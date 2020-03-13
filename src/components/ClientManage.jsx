e612633fcd0bca63e0d4e11db90f5a2e24907d2b
acatalin92 <a.catalin92@gmail.com>
2020-03-13 12:56:54 +0200

    push before demo

--- src/components/ClientManage.jsx
+++ src/components/ClientManage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import styles from './ClientManage.module.css';
 import Button from './Button';
@@ -50,35 +50,87 @@ const ClientManage = props => {
   const company_name = useField('company_name', form);
   const type = useField('type', form);
 
+  const contact_name = useField('contact_name', form);
+  const contact_title = useField('contact_title', form);
+  const contact_phone = useField('contact_phone', form);
+  const contact_fax = useField('contact_fax', form);
+  const contact_email = useField('contact_email', form);
+
   return (
     <div className={styles.container}>
       <div className={styles.section}>
-        <div className={styles.title}>
-          <span>Name and type</span>
-        </div>
         <form className={styles.form} onSubmit={handleSubmit}>
-          <Input
-            className={styles.formControl}
-            field={name}
-            label="Client name *"
-          />
-          <Input
-            className={styles.formControl}
-            field={company_name}
-            label="Company name *"
-          />
-          <Select
-            className={styles.formControl}
-            field={type}
-            options={clientTypesOptions}
-            placeholder={!!data ? 'UNASSIGNED' : 'Contact type...'}
-            label="Client type *"
-          />
-          <div className={styles.title}>
-            <span>Primary contact</span>
+          <div className={styles.formSection}>
+            <div className={styles.title}>
+              <span>Name and type</span>
+            </div>
+            <div className={styles.controlsGroup}>
+              <Input
+                className={styles.formControl}
+                field={name}
+                label="Client name *"
+              />
+              <Input
+                className={styles.formControl}
+                field={company_name}
+                label="Company name *"
+              />
+            </div>
+            <Select
+              className={styles.formControl}
+              field={type}
+              options={clientTypesOptions}
+              placeholder={!!data ? 'UNASSIGNED' : 'Contact type...'}
+              label="Client type *"
+            />
           </div>
-          <div className={styles.title}>
-            <span>Mailing address</span>
+          <div className={styles.formSection}>
+            <div className={styles.title}>
+              <span>Primary contact</span>
+            </div>
+            <div className={styles.controlsGroup}>
+              <Input
+                className={styles.formControl}
+                field={contact_name}
+                label="Name *"
+              />
+              <Input
+                className={styles.formControl}
+                field={contact_title}
+                label="Title"
+              />
+            </div>
+            <div className={styles.controlsGroup}>
+              <Input
+                className={styles.formControl}
+                field={contact_phone}
+                label="Phone number *"
+              />
+              <Input
+                className={styles.formControl}
+                field={contact_fax}
+                label="Fax number"
+              />
+            </div>
+            <Input
+              className={styles.formControl}
+              field={contact_email}
+              label="Email *"
+            />
+          </div>
+          <div className={styles.formSection}>
+            <div className={styles.controlsGroup}>
+              <div>
+                <div className={styles.title}>
+                  <span>Mailing address</span>
+                </div>
+              </div>
+              <div>
+                <div className={styles.title}>
+                  <span>Billing address</span>
+                </div>
+              </div>
+            </div>
           </div>
           <div className={styles.formButtons}>
             <Button type="submit" disabled={submitting}>
