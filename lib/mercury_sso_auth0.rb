9a7659a5756064180f56f67521807e421e93ef23
Relu Burlan <aurelian.burlan@e-spres-oh.com>
2020-05-18 16:02:06 +0300

    Auth0 configuration

--- lib/mercury_sso_auth0.rb
+++ lib/mercury_sso_auth0.rb
@@ -1,5 +1,20 @@
 require "mercury_sso_auth0/engine"
 
 module MercurySsoAuth0
-  # Your code goes here...
+
+  # Auth0 Client ID
+  mattr_accessor :client
+  @@client = nil
+
+  # Auth0 Client secret
+  mattr_accessor :secret
+  @@secret = nil
+
+  # Auth0 Client domain
+  mattr_accessor :domain
+  @@domain = nil
+
+  def self.setup
+    yield self
+  end
 end
