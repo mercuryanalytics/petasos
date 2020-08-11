c0217573e6714c86b15fdc0ba9b3f8cae33b1005
Relu Burlan <aurelian.burlan@e-spres-oh.com>
2020-08-11 12:46:58 +0300

    Return roles for user templates

--- app/serializers/v1/default_template/project_serializer.rb
+++ app/serializers/v1/default_template/project_serializer.rb
@@ -5,6 +5,7 @@ module V1
       attribute :name
       attribute :authorized
       attribute :reports
+      attribute :roles
 
       def reports
         self.object.reports.map do |report|
@@ -16,6 +17,22 @@ module V1
         authorizations.select do |authorization|
           authorization.subject_class == 'Project' && authorization.subject_id == self.object.id
         end.any?
+      end
+
+      def roles
+        authorization = authorizations.find do |authorization|
+          authorization.subject_class == 'Project' && authorization.subject_id == object.id
+        end
+
+        return [] unless authorization
+
+        scopes = authorization.scopes
+
+        roles = []
+        roles << "project_access" if scopes.map(&:action).include?('access')
+        roles << "project_admin" if scopes.map(&:action).include?('authorize')
+        roles << "project_editor" if scopes.map(&:action).include?('update')
+        roles
       end
     end
   end
