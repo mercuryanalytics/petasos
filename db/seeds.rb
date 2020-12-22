3b52f6b3998051546e4e4a092c7f8699ecedc792
Scott Brickner <scottb@brickner.net>
2020-12-22 16:35:39 -0500

    Add creation of global scope to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
-client = Client.first
-client2 = Client.last
-
-project = Project.create!(client: client, name: 'Client 1 project 1')
-c1_p1_r1 = Report.create!(project: project, name: 'Client 1 project 1 report 1')
-c1_p1_r2 = Report.create!(project: project, name: 'Client 1 project 1 report 2')
-c1_p1_r3 = Report.create!(project: project, name: 'Client 1 project 1 report 3')
-
-
-c1_p2 = Project.create!(client: client, name: 'Client 1 project 2')
-c1_p2_r1 = Report.create!(project: c1_p2, name: 'Client 1 project 2 report 1')
-c1_p2_r2 = Report.create!(project: c1_p2, name: 'Client 1 project 2 report 2')
-c1_p2_r3 = Report.create!(project: c1_p2, name: 'Client 1 project 2 report 3')
-
-
-client_2 = Client.last
-c2_p1 = Project.create!(client: client_2, name: 'Client 2 project 1')
-c2_p1_r1 = Report.create!(project: c2_p1, name: 'Client 2 project 2 report 1')
-c2_p1_r2 = Report.create!(project: c2_p1, name: 'Client 2 project 2 report 2')
-c2_p1_r3 = Report.create!(project: c2_p1, name: 'Client 2 project 2 report 3')
-
-c2_p2 = Project.create!(client: client_2, name: 'Client 2 project 2')
-c2_p2_r1 = Report.create!(project: c2_p2, name: 'Client 2 project 2 report 1')
-c2_p2_r2 = Report.create!(project: c2_p2, name: 'Client 2 project 2 report 2')
-c2_p2_r3 = Report.create!(project: c2_p2, name: 'Client 2 project 2 report 3')
-
-user = User.first
-
-Authorization.create!(user: user, subject_class: 'Report', subject_id: c2_p2_r1.id)
-Authorization.create!(user: user, subject_class: 'Project', subject_id: c2_p1.id)
-Authorization.create!(user: user, subject_class: 'Project', subject_id: c1_p2.id)
+Scope.create(scope: "admin", action: "admin", description: "Global admin", global: true, dynamic: false, name: "Mercury Analytics Admin")
