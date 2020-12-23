647983dc6bf672547c95d29e81223d8e0b6d8cf8
Matt Williams <matt.seongjun@gmail.com>
2020-12-22 23:08:17 -0500

    Change auth to researchresultswebsite

--- src/auth-config.js
+++ src/auth-config.js
@@ -4,7 +4,7 @@ import { FcGoogle } from 'react-icons/fc';
 import { FaWindows } from 'react-icons/fa';
 
 const authConfig = {
-  domain: 'auth.ratethedebate.com',
+  domain: 'auth.researchresultswebsite.com',
   clientId: 'gGZDDRXWMgdEzA5RaWzT8yR0ysvOce0B',
   loginUrl: `${Constants.APP_URL}${Routes.Login}`,
   callbackUrl: `${Constants.APP_URL}${Routes.LoginCallback}`,
