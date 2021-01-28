3e5f31c92d41c0f012869ade489498338dd5f591
reluburlan <59829211+reluburlan@users.noreply.github.com>
2021-01-28 15:57:19 +0200

    #37 - downcase user email (#43)

--- app/interactors/users/validate_user.rb
+++ app/interactors/users/validate_user.rb
@@ -5,7 +5,8 @@ module Users
     delegate :params, :user, to: :context
 
     def call
-      user = context.user || User.find_or_initialize_by(email: params[:email].downcase!)
+      params[:email].downcase!
+      user = context.user || User.find_or_initialize_by(email: params[:email])
       user.assign_attributes(params.except(:password, :client_id))
       user.new_record? ? context.new_user = 1 : context.new_user = 0
       user.valid? ? context.user = user : context.fail!(message: user.errors)
