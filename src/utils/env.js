5e5df53088aa56ee3c4552222b9538f16633efe7
Scott Brickner <scottb@brickner.net>
2022-06-14 14:25:57 -0400

    Remove staging endpoints

--- src/utils/env.js
+++ src/utils/env.js
@@ -4,21 +4,9 @@ const url = `${host.indexOf('localhost') === 0 ? 'http' : 'https'}://${host}`;
 export const EnvTypes = {
   PRODUCTION: 'production',
   DEVELOPMENT: 'development',
-  STAGING_HEROKU: 'staging-heroku',
-  STAGING_AWS: 'staging-aws',
 };
 
-let Env = {
-  type: EnvTypes.DEVELOPMENT,
+export default {
+  type: host.indexOf('localhost') !== 0 ? EnvTypes.PRODUCTION : EnvTypes.DEVELOPMENT,
   publicUrl: url,
 };
-
-if (host.indexOf('herokuapp.com') > -1) {
-  Env.type = EnvTypes.STAGING_HEROKU;
-} else if (host.indexOf('aurelianb.com') > -1) {
-  Env.type = EnvTypes.STAGING_AWS;
-} else if (host.indexOf('localhost') !== 0) {
-  Env.type = EnvTypes.PRODUCTION;
-}
-
-export default Env;
