14dcc47cb601567c79b0046abbe39e92f802ae46
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-01-24 23:55:00 +0530

    wip: create a menu structure using react aria tree for side menu

--- ui2/src/components/navigation_menu/index.tsx
+++ ui2/src/components/navigation_menu/index.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import Search from "./search"
 import "./index.scss"
-import Client from "./client"
+// import Client from "./client"
+import MenuStructure from "./menu_structure"
 
 const NavigationMenu: React.FC<{ showSideMenu: boolean }> = ({ showSideMenu }) => {
   return (
     <div className={`Navigation-Menu${showSideMenu ? "" : " slide"}`}>
       <Search />
-      <Client />
+      {/* <Client /> */}
+      <MenuStructure />
     </div>
   )
 }
