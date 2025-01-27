32cba129548c806fc1e56686a30c2aa8905c7a83
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-01-28 01:36:59 +0530

    feat: create basic form structure with body having react aria tabs and title

---
+++ ui2/src/components/form/body/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Tabs from "./tabs"
+
+const Body: React.FC = () => {
+  return (
+    <div className="Form-Body">
+      <Tabs />
+      <div>Main Form</div>
+    </div>
+  )
+}
+
+export default Body
