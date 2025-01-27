8eca1086e53c698dd477f155829803d6593a8f2d
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-01-28 03:46:26 +0530

    wip: create basic structure for client details form

---
+++ ui2/src/components/form/body/client_details/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./index.scss"
+
+const ClientDetails: React.FC = () => {
+  return (
+    <div className="Client-Details">
+      <div>Name and Type</div>
+      <div>Client Name</div>
+    </div>
+  )
+}
+
+export default ClientDetails
