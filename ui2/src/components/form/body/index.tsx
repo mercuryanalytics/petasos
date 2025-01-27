a070d5eed28d246a290b6a4f920b6db55933b646
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-01-28 03:46:26 +0530

    wip: create basic structure for client details form

--- ui2/src/components/form/body/index.tsx
+++ ui2/src/components/form/body/index.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 
 import Tabs from "./tabs"
+import ClientDetails from "./client_details"
 
 const Body: React.FC = () => {
   return (
     <div className="Form-Body">
       <Tabs />
-      <div>Main Form</div>
+      <ClientDetails />
     </div>
   )
 }
