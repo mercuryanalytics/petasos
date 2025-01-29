05f824c258a998ad1d6fd23cb3113551a4b06b7b
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-01-30 01:44:51 +0530

    feat: create basic selectbox and stylesheets for name and type column

---
+++ ui2/src/components/form/body/client_details/info/name_and_type/select_box/index.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { Button, Label, ListBox, ListBoxItem, Popover, Select, SelectValue } from "react-aria-components"
+import { ArrowDown } from "../../../../../../icons"
+
+import "./index.scss"
+
+const Selectbox: React.FC = () => (
+  <div>
+    <Select>
+      <Label>Partner Type</Label>
+      <Button slot="chevron">
+        <SelectValue>
+          {({ defaultChildren, isPlaceholder }) => {
+            return isPlaceholder ? <span>Partner</span> : defaultChildren
+          }}
+        </SelectValue>
+        <ArrowDown />
+      </Button>
+      <Popover>
+        <ListBox>
+          <ListBoxItem>Client</ListBoxItem>
+          <ListBoxItem>Partner</ListBoxItem>
+          <ListBoxItem>Other</ListBoxItem>
+        </ListBox>
+      </Popover>
+    </Select>
+  </div>
+)
+
+export default Selectbox
