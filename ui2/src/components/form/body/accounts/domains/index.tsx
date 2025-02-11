a1d8a09af675fe80c2106295bfdf31712542140e
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-12 02:26:51 +0530

    feat: add basic state to switch client  and accounts panels in body

---
+++ ui2/src/components/form/body/accounts/domains/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Domains: React.FC = () => {
+  return <div>index</div>
+}
+
+export default Domains
---
+++ ui2/src/components/form/body/accounts/users/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Users: React.FC = () => {
+  return <div>index</div>
+}
+
+export default Users
