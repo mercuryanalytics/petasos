0f8fe5f46eaf762ca049b37951f9a0779f0def46
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-12 02:29:00 +0530

    wip: create basic client names data list

---
+++ ui2/public/clientNames.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    title: "Client1",
+    children: [
+      { id: 2, title: "name 1", children: [] },
+      { id: 3, title: "name 2", children: [] }
+    ]
+  },
+  {
+    id: 4,
+    title: "Client2",
+    children: [
+      { id: 5, title: "name 3", children: [] },
+      { id: 6, title: "name 4", children: [] }
+    ]
+  }
+]
