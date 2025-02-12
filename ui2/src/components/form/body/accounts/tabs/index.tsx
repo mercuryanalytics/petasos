00be64fcac3a4adeb900100c902d0b93654ed84b
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-13 00:45:37 +0530

    refactor: small change

--- ui2/src/components/form/body/accounts/tabs/index.tsx
+++ ui2/src/components/form/body/accounts/tabs/index.tsx
@@ -3,17 +3,15 @@ import { Tabs, TabList, Tab } from "react-aria-components"
 
 import "./index.scss"
 
-const AccountsTabs: React.FC = () => {
-  return (
-    <div className="AccountsTabs">
-      <Tabs>
-        <TabList aria-label="users and domains">
-          <Tab id="users">Users</Tab>
-          <Tab id="domains">Domains</Tab>
-        </TabList>
-      </Tabs>
-    </div>
-  )
-}
+const AccountsTabs: React.FC = () => (
+  <div className="AccountsTabs">
+    <Tabs>
+      <TabList aria-label="users and domains">
+        <Tab id="users">Users</Tab>
+        <Tab id="domains">Domains</Tab>
+      </TabList>
+    </Tabs>
+  </div>
+)
 
 export default AccountsTabs
