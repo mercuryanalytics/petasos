639c55065e33cca2600a0d689dbc9b3c5be13553
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-14 02:23:40 +0530

    feat: edit button that shows input fields in user info form

--- ui2/src/components/form/body/accounts/users/user_info/index.tsx
+++ ui2/src/components/form/body/accounts/users/user_info/index.tsx
@@ -1,4 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
+
+import { Pen } from "../../../../../icons"
 
 import AccountDetails from "./account_details"
 import Contact from "./contact"
@@ -6,10 +8,20 @@ import Contact from "./contact"
 import "./index.scss"
 
 const UserInfo: React.FC = () => {
+  const [showInput, setShowInput] = useState(false)
+
   return (
     <form className="UserInfo">
-      <AccountDetails />
-      <Contact />
+      <div>
+        <AccountDetails showInput={showInput} />
+        <Contact showInput={showInput} />
+      </div>
+      {!showInput && (
+        <button onClick={() => setShowInput(true)}>
+          <h1>Edit</h1>
+          <Pen />
+        </button>
+      )}
     </form>
   )
 }
