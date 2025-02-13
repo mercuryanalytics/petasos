490a3df8c05eae010c5f30ab64a2be03ef1b6fde
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-13 23:12:50 +0530

    feat: create component for modifying user data in accounts

--- ui2/src/components/form/body/accounts/users/modify_user/index.tsx
+++ ui2/src/components/form/body/accounts/users/modify_user/index.tsx
@@ -1,7 +1,11 @@
 import React from "react"
-import { Input, Label, SearchField } from "react-aria-components"
+import { Input, Label, SearchField, Tabs, TabList, Tab } from "react-aria-components"
+import SimpleBarReact from "simplebar-react"
+
+import { Trash } from "../../../../../icons"
 
 import "./index.scss"
+import range from "../../../../../../util/range"
 
 const ModifyUser: React.FC = () => {
   return (
@@ -13,6 +17,19 @@ const ModifyUser: React.FC = () => {
         <Label />
         <Input placeholder="Search" />
       </SearchField>
+      <button>+ Add User</button>
+      <SimpleBarReact style={{ height: "530px" }}>
+        <Tabs>
+          <TabList aria-label="Users List">
+            {/* TODO: Change this later when using real data */}
+            {Array.from(range(0, 20)).map((_, i) => (
+              <Tab id={`name${i}`}>
+                <span>name{i}</span> <Trash />
+              </Tab>
+            ))}
+          </TabList>
+        </Tabs>
+      </SimpleBarReact>
     </div>
   )
 }
