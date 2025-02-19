1a1e32453185274da10538b7e958e87f8f973b27
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-20 03:00:13 +0530

    feat: add functionality and styles when user switches to domains tab in accounts

--- ui2/src/components/form/body/accounts/index.tsx
+++ ui2/src/components/form/body/accounts/index.tsx
@@ -1,14 +1,35 @@
-import React from "react"
-import AccountsTabs from "./tabs"
+import React, { useState } from "react"
+
+import Users from "./users"
+import CustomAccountTabs from "./custom_account_tabs"
 
 import "./index.scss"
-import Users from "./users"
+import DomainInfo from "./domains/domain_info"
+import ModifyDomain from "./domains/modify_domain"
 
 const Accounts: React.FC = () => {
+  const [showUserTabs, setShowUserTabs] = useState("users")
+  const [showUserInfo, setShowUserInfo] = useState("user info")
+
   return (
     <div className="Accounts">
-      <AccountsTabs />
-      <Users />
+      <CustomAccountTabs tab1="Users" tab2="Domains" setShowUserTabs={setShowUserTabs} />
+      {showUserTabs === "users" ? (
+        <>
+          <CustomAccountTabs
+            className="UserInfoPermissions"
+            tab1="User info"
+            tab2="Access and Permissions"
+            setShowUserInfo={setShowUserInfo}
+          />
+          <Users showUserInfo={showUserInfo} />
+        </>
+      ) : (
+        <>
+          <DomainInfo />
+          <ModifyDomain />
+        </>
+      )}
     </div>
   )
 }
