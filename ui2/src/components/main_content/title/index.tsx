4f53c1b35d640dea4de2463bd2a5da135627f603
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-27 23:22:33 +0530

    refactor: change name of folders

--- ui2/src/components/main_content/title/index.tsx
+++ ui2/src/components/main_content/title/index.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import "./index.scss"
 
 const Title: React.FC = () => (
-  <div className="FormTitle">
+  <div className="MainContentTitle">
     <span>title</span>
   </div>
 )
