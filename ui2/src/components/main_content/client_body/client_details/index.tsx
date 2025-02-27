1295d4416925778631d6720b1cd362eebefd95dd
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-02-28 01:56:36 +0530

    refactor: move select_user component to common and add name prop

--- ui2/src/components/main_content/client_body/client_details/index.tsx
+++ ui2/src/components/main_content/client_body/client_details/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Form } from "react-aria-components"
 
 import Info from "./info"
-import SelectUser from "./select_user"
+import SelectUser from "../../../common/select_user"
 
 import "./index.scss"
 
