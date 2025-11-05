c001654e0d6a9fd0571adc21c6af11821724cfa4
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-11-05 20:54:09 +0530

    refactor: small code refators

--- ui2/src/components/main_content/project_body/project_details/footer/index.tsx
+++ ui2/src/components/main_content/project_body/project_details/footer/index.tsx
@@ -1,14 +1,23 @@
 import React from "react"
+import { useSetAtom } from "jotai"
+
+import { showInput } from "../../../../../atoms"
 
 import "./index.scss"
 
-const ProjectDetailsFooter: React.FC<{ setShowInput: React.Dispatch<React.SetStateAction<boolean>> }> = ({
-  setShowInput
-}) => (
-  <div className="ProjectDetailsFooter">
-    <button>Update</button>
-    <button onClick={() => setShowInput(false)}>Cancel</button>
-  </div>
-)
+const ProjectDetailsFooter: React.FC = () => {
+  const setShowInput = useSetAtom(showInput)
+
+  return (
+    <>
+      {showInput && (
+        <div className="ProjectDetailsFooter">
+          <button>Update</button>
+          <button onClick={() => setShowInput(false)}>Cancel</button>
+        </div>
+      )}
+    </>
+  )
+}
 
 export default ProjectDetailsFooter
--- ui2/src/components/navigation_menu/menu/index.tsx
+++ ui2/src/components/navigation_menu/menu/index.tsx
@@ -1,38 +1,52 @@
 import React from "react"
+import { useSetAtom } from "jotai"
 
 import { UNSTABLE_Tree as Tree, UNSTABLE_TreeItem as TreeItem, Button } from "react-aria-components"
 
 import items from "../../../../public/menuItems"
+
+import { showInput } from "../../../atoms"
 
 import InsertItem from "./insert_item"
 import Content from "./Content"
 
 import "./index.scss"
 
-const Menu: React.FC = () => (
-  <div className="Menu">
-    <Tree aria-label="Files" selectionMode="multiple" items={items}>
-      {function renderItem({ title, children }) {
-        return (
-          <TreeItem textValue={title}>
-            <Content title={title} children={children} />
-            {children.map((item, i) => {
-              const { title } = item
-              return (
-                <React.Fragment key={`${title}_${i}_child`}>
-                  {i === 0 && (title === "Project" || title === "Report") && <InsertItem title={title} />}
-                  {renderItem(item)}
-                </React.Fragment>
-              )
-            })}
-          </TreeItem>
-        )
+const Menu: React.FC = () => {
+  const setShowInput = useSetAtom(showInput)
+
+  return (
+    <div
+      className="Menu"
+      // FIXME: Check if the project name or number is same as the body of the clicked element then dont setshowInput to false
+      onClickCapture={() => {
+        console.log("hello")
+        setShowInput(false)
       }}
-    </Tree>
-    <Button>
-      <a href="#">+ Create New Client</a>
-    </Button>
-  </div>
-)
+    >
+      <Tree aria-label="Files" selectionMode="multiple" items={items}>
+        {function renderItem({ title, children }) {
+          return (
+            <TreeItem textValue={title}>
+              <Content title={title} children={children} />
+              {children.map((item, i) => {
+                const { title } = item
+                return (
+                  <React.Fragment key={`${title}_${i}_child`}>
+                    {i === 0 && (title === "Project" || title === "Report") && <InsertItem title={title} />}
+                    {renderItem(item)}
+                  </React.Fragment>
+                )
+              })}
+            </TreeItem>
+          )
+        }}
+      </Tree>
+      <Button>
+        <a href="#">+ Create New Client</a>
+      </Button>
+    </div>
+  )
+}
 
 export default Menu
