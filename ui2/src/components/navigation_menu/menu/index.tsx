da71440e48a246c89f509de28346374acecaf85d
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-11-12 01:04:45 +0530

    wip: add simplebar to navigation menu; use reference number for tree items; change styles

--- ui2/src/components/navigation_menu/menu/index.tsx
+++ ui2/src/components/navigation_menu/menu/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import SimpleBarReact from "simplebar-react"
 import { useSetAtom } from "jotai"
 
 import { UNSTABLE_Tree as Tree, UNSTABLE_TreeItem as TreeItem, Button } from "react-aria-components"
@@ -6,6 +7,8 @@ import { UNSTABLE_Tree as Tree, UNSTABLE_TreeItem as TreeItem, Button } from "re
 import items from "../../../../public/menuItems"
 
 import { showInput } from "../../../atoms"
+
+import { MenuItem } from "../../common/types"
 
 import InsertItem from "./insert_item"
 import Content from "./Content"
@@ -16,35 +19,39 @@ const Menu: React.FC = () => {
   const setShowInput = useSetAtom(showInput)
 
   return (
-    <div
-      className="Menu"
-      // FIXME: Check if the project name or number is same as the body of the clicked element then dont setshowInput to false
-      onClickCapture={() => {
-        setShowInput(false)
-      }}
-    >
-      <Tree aria-label="Files" selectionMode="multiple" items={items}>
-        {function renderItem({ type, title, children }) {
-          return (
-            <TreeItem textValue={title}>
-              <Content type={type} title={title} children={children} />
-              {children.map((item, i) => {
-                const { type } = item
-                return (
-                  <React.Fragment key={`${type}_${i}_child`}>
-                    {i === 0 && (type === "Project" || type === "Report") && <InsertItem type={type} />}
-                    {renderItem(item)}
-                  </React.Fragment>
-                )
-              })}
-            </TreeItem>
-          )
+    <SimpleBarReact style={{ height: "calc(100% - 59px)" }}>
+      <div
+        className="Menu"
+        // FIXME: Check if the project name or number is same as the body of the clicked element then dont setshowInput to false
+        onClickCapture={() => {
+          setShowInput(false)
         }}
-      </Tree>
-      <Button>
-        <a href="#">+ Create New Client</a>
-      </Button>
-    </div>
+      >
+        <Tree aria-label="Files" items={items}>
+          {function renderItem({ type, title, reference, children }: MenuItem) {
+            return (
+              <TreeItem textValue={title}>
+                <Content type={type} title={title} reference={reference} children={children} />
+                {children.map((item, i) => {
+                  const { type } = item
+                  return (
+                    <React.Fragment key={`${type}_${i}_child`}>
+                      {i === 0 && (type === "projects" || type === "reports") && (
+                        <InsertItem type={type} reference={reference} />
+                      )}
+                      {renderItem(item)}
+                    </React.Fragment>
+                  )
+                })}
+              </TreeItem>
+            )
+          }}
+        </Tree>
+        <Button>
+          <a href="#">+ Create New Client</a>
+        </Button>
+      </div>
+    </SimpleBarReact>
   )
 }
 
