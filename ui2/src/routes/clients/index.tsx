50061ba94af95b2773672874bcb81bb7a5e9ca9b
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-11-12 07:33:21 +0530

    cleanup: remove clients.projects,reports index page; remove common home component; add not found component to root

--- ui2/src/routes/clients/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router"
-import { Home } from "../../components/common/Home"
-
-export const Route = createFileRoute("/clients/")({
-  component: () => <Home />
-})
