1444629a283ec18e72fdc35c54ed217ba3d29621
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-11-18 17:38:55 +0530

    feat: create util method to redirect to a not found page for clients, projects and reports urls

--- ui2/src/components/common/router/util.ts
+++ ui2/src/components/common/router/util.ts
@@ -1,12 +1,34 @@
-import { redirect } from "@tanstack/react-router"
-import { MenuItem } from "../types"
+import { ParsedLocation, redirect } from "@tanstack/react-router"
 
-export const matchReference = (
+import menuItems from "../../../../public/menuItems"
+
+const clients = menuItems.filter(item => item.type === "clients")
+const projects = clients.flatMap(item => item.children.filter(item => item.type === "projects"))
+const reports = projects.flatMap(item => item.children.filter(item => item.type === "reports"))
+
+export const redirectTo = (
   params: {
-    clientId: string
+    clientId?: string
+    projectId?: string
+    reportId?: string
   },
-  data: MenuItem[]
+
+  location: ParsedLocation<object>
 ) => {
-  if (!data.some(item => item.type === "clients" && item.reference === params.clientId))
-    throw redirect({ to: `/${params.clientId}` })
+  const type = location.href.split("/")[1]
+
+  switch (type) {
+    case "clients": {
+      if (!clients.some(item => item.reference === params.clientId)) throw redirect({ to: `/${params.clientId}` })
+      break
+    }
+    case "projects": {
+      if (!projects.some(item => item.reference === params.projectId)) throw redirect({ to: `/${params.projectId}` })
+      break
+    }
+    default: {
+      if (!reports.some(item => item.reference === params.reportId)) throw redirect({ to: `/${params.reportId}` })
+      break
+    }
+  }
 }
--- ui2/src/routes/projects/$projectId.tsx
+++ ui2/src/routes/projects/$projectId.tsx
@@ -1,8 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router"
 import ProjectBody from "../../components/main_content/project_body"
+import { redirectTo } from "../../components/common/router/util"
 
 const Project = () => <ProjectBody />
 
 export const Route = createFileRoute("/projects/$projectId")({
+  beforeLoad: ({ params, location }) => {
+    redirectTo(params, location)
+  },
   component: Project
 })
