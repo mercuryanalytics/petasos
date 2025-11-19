b402771bd7ded48e4c1d6dcaa293cbb006784b2e
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-11-19 10:53:25 +0530

    refactor: remove unnecessary code for new report and project routes

--- ui2/src/routes/reports/new/$projectId.tsx
+++ ui2/src/routes/reports/new/$projectId.tsx
@@ -1,8 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router"
 import NewReportBody from "../../../components/main_content/new_report_body"
 
-const NewReport = () => <NewReportBody />
-
 export const Route = createFileRoute("/reports/new/$projectId")({
-  component: NewReport
+  component: () => <NewReportBody />
 })
