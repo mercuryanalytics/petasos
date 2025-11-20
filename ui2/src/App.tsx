37e464fc3bfec860a7c3e2c17308310f1e1e5571
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-11-20 20:50:29 +0530

    wip: add functionality to check global click and hide search field

--- ui2/src/App.tsx
+++ ui2/src/App.tsx
@@ -1,8 +1,11 @@
-import React, { PropsWithChildren, useState } from "react"
+import React, { PropsWithChildren, useEffect, useState } from "react"
+import { useSetAtom } from "jotai"
 
 import Navbar from "./components/navbar"
 import NavigationMenu from "./components/navigation_menu"
 import MainContent from "./components/main_content"
+
+import { search } from "./atoms"
 
 import "./styles/react_aria_checkbox.scss"
 import "./styles/react_aria_search_field.scss"
@@ -14,6 +17,20 @@ import "./App.scss"
 
 const App: React.FC<PropsWithChildren> = ({ children }) => {
   const [showSideMenu, setSideShowMenu] = useState(true)
+  const setSeatch = useSetAtom(search)
+
+  useEffect(() => {
+    const handleGlobalClickCapture = (event: MouseEvent) => {
+      const target = event.target as HTMLInputElement
+
+      if (!target.className.includes("NavigationInput")) {
+        setSeatch(false)
+      }
+    }
+
+    document.addEventListener("click", handleGlobalClickCapture, true)
+    return () => document.removeEventListener("click", handleGlobalClickCapture, true)
+  }, [setSeatch])
 
   return (
     <>
