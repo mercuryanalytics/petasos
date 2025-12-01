65649139c3f2c6394112105ae9c328d82c1a9204
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-12-01 22:11:20 +0530

    refactor: small refactor for nav menu items

--- ui2/src/atoms/index.ts
+++ ui2/src/atoms/index.ts
@@ -1,6 +1,8 @@
 import { atom } from "jotai"
 
+import { MenuItem } from "../components/common/types"
+
 export const showInput = atom(false)
 export const showSearch = atom(false)
 export const hideClients = atom(false)
-export const searchValue = atom<string>()
+export const menuItems = atom<MenuItem[]>()
