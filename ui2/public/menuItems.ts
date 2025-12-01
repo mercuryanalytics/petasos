9e46a968c43f777554afc680f26728f53d9fca1f
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-12-01 14:10:53 +0530

    wip:give unique names to to all records and remove unnecessary title property

--- ui2/public/menuItems.ts
+++ ui2/public/menuItems.ts
@@ -2,27 +2,24 @@ export default [
   {
     id: 1,
     type: "clients",
-    title: "New Client 1",
-    name: "client1",
+    name: "New Client 1",
     reference: "0",
     children: [
       {
         id: 2,
         type: "projects",
-        title: "Test",
         name: "project1",
         reference: "0000",
-        children: [{ id: 3, type: "reports", title: "Test Results", name: "report1", reference: "001", children: [] }]
+        children: [{ id: 3, type: "reports", name: "report1", reference: "001", children: [] }]
       },
       {
         id: 4,
         type: "projects",
-        title: "Test",
         name: "project2",
         reference: "0001",
         children: [
-          { id: 5, type: "reports", title: "Test Results", name: "report2", reference: "002", children: [] },
-          { id: 6, type: "reports", title: "Test Results", name: "report3", reference: "003", children: [] }
+          { id: 5, type: "reports", name: "report2", reference: "002", children: [] },
+          { id: 6, type: "reports", name: "report3", reference: "003", children: [] }
         ]
       }
     ]
@@ -30,27 +27,24 @@ export default [
   {
     id: 7,
     type: "clients",
-    title: "New Client 2",
-    name: "client2",
+    name: "New Client 2",
     reference: "1",
     children: [
       {
         id: 8,
         type: "projects",
-        title: "Test",
         name: "project3",
         reference: "1110",
-        children: [{ id: 9, type: "reports", title: "Test Results", name: "report4", reference: "111", children: [] }]
+        children: [{ id: 9, type: "reports", name: "report4", reference: "111", children: [] }]
       },
       {
         id: 10,
         type: "projects",
-        title: "Test",
         name: "project4",
         reference: "1111",
         children: [
-          { id: 11, type: "reports", title: "Test Results", name: "report5", reference: "112", children: [] },
-          { id: 12, type: "reports", title: "Test Results", name: "report6", reference: "113", children: [] }
+          { id: 11, type: "reports", name: "report5", reference: "112", children: [] },
+          { id: 12, type: "reports", name: "report6", reference: "113", children: [] }
         ]
       }
     ]
