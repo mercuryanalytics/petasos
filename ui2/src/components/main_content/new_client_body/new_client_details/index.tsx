eb1a91a9be8dc37fcb18e9c78e62b2ce5a60d2a5
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-12-11 18:50:25 +0530

    wip: create complete new client details component with form submit and cancel functionalities

--- ui2/src/components/main_content/new_client_body/new_client_details/index.tsx
+++ ui2/src/components/main_content/new_client_body/new_client_details/index.tsx
@@ -1,7 +1,32 @@
-import React from "react"
+import React, { FormEvent } from "react"
+
+import NewClientLogo from "./logo"
+import NewClientInfo from "./info"
+import NewClientFooter from "./footer"
+
+import "./index.scss"
+
+const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  event.preventDefault()
+  const formData = new FormData(event.currentTarget)
+  console.log(Object.fromEntries(formData.entries()))
+}
+
+const handleReset = (event: FormEvent<HTMLFormElement>) => {
+  event.preventDefault()
+  event.currentTarget.reset()
+}
 
 const NewClientDetails: React.FC = () => {
-  return <div>New Client Details</div>
+  return (
+    <div className="NewClientDetails">
+      <form onSubmit={handleSubmit} onReset={handleReset}>
+        <NewClientLogo />
+        <NewClientInfo />
+        <NewClientFooter />
+      </form>
+    </div>
+  )
 }
 
 export default NewClientDetails
