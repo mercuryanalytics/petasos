fd6065178c35c7a012d8e15d191f6b3d68ab0dc6
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-12-18 00:01:42 +0530

    refactor: use context to get name out of record

--- ui2/src/routes/reports/$reportId.tsx
+++ ui2/src/routes/reports/$reportId.tsx
@@ -3,9 +3,7 @@ import ReportBody from "../../components/main_content/report_body"
 import { findRecord } from "../-util"
 
 export const Route = createFileRoute("/reports/$reportId")({
-  beforeLoad: ({ params, location }) => {
-    findRecord(params, location)
-  },
-  loader: ({ params, location }) => findRecord(params, location).name,
+  beforeLoad: ({ params, location }) => ({ record: findRecord(params, location) }),
+  loader: ({ context }) => context.record.name,
   component: () => <ReportBody />
 })
