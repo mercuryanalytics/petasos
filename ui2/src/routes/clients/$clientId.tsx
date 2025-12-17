cd409777dde807654103c3f22d4f18a1537bd90d
Amit Pathak <64804304+AmitP1585@users.noreply.github.com>
2025-12-18 00:01:42 +0530

    refactor: use context to get name out of record

--- ui2/src/routes/clients/$clientId.tsx
+++ ui2/src/routes/clients/$clientId.tsx
@@ -3,10 +3,7 @@ import ClientBody from "../../components/main_content/client_body"
 import { findRecord } from "../-util"
 
 export const Route = createFileRoute("/clients/$clientId")({
-  // FIXME: Maybe find a betterway to use record with redirect to for before load and loader
-  beforeLoad: ({ params, location }) => {
-    findRecord(params, location)
-  },
-  loader: ({ params, location }) => findRecord(params, location).name,
+  beforeLoad: ({ params, location }) => ({ record: findRecord(params, location) }),
+  loader: ({ context }) => context.record.name,
   component: () => <ClientBody />
 })
--- ui2/src/routes/projects/$projectId.tsx
+++ ui2/src/routes/projects/$projectId.tsx
@@ -3,9 +3,7 @@ import ProjectBody from "../../components/main_content/project_body"
 import { findRecord } from "../-util"
 
 export const Route = createFileRoute("/projects/$projectId")({
-  beforeLoad: ({ params, location }) => {
-    findRecord(params, location)
-  },
-  loader: ({ params, location }) => findRecord(params, location).name,
+  beforeLoad: ({ params, location }) => ({ record: findRecord(params, location) }),
+  loader: ({ context }) => context.record.name,
   component: () => <ProjectBody />
 })
